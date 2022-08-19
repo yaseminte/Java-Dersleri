db2f93aeec68cbc3f1d5564724472ef6b4c01b21
Yasemin Turker <yturker89@gmail.com>
2022-08-19 17:35:27 +0300

    19.08.2022

---
+++ src/day49_lambda/Utils.java
@@ -0,0 +1,32 @@
+package day49_lambda;
+
+public class Utils {
+
+    public static void ayniSatirdaBosluklaYazdir(Object obj){
+
+        System.out.print(obj + " ");
+
+    }
+
+    public static boolean ciftElemaniSec(int x){
+
+
+        return x%2==0;
+    }
+
+    public static boolean tekElemaniSec(int x){
+
+
+        return x%2!=0;
+    }
+
+    public static int karesiniAl(int x){
+
+        return x*x;
+    }
+    public static int kupunuAl(int x){
+
+        return x*x*x;
+    }
+
+}
